0c1d054de57b2fedd24f19118f92076a0b17dc73
Matt Chowning <mchowning@gmail.com>
2017-08-23 11:40:04 -0400

    Adding java

---
+++ java/src/main/java/NinetyNineBottles.java
@@ -0,0 +1,14 @@
+class NinetyNineBottles {
+
+    String song() {
+        return null;
+    }
+
+    String verses(int start, int end) {
+        return null;
+    }
+
+    String verse(int num) {
+        return null;
+    }
+}
